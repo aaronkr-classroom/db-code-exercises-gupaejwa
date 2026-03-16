94be8b40561ca1a4c4d312381992f558456b2af0
gupaejwa <sin010423@naver.com>
2026-03-16 15:21:12 +0900

    Add files via upload

---
+++ code/week2.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE ut;
+
+CREATE TABLE animals(
+	id bigserial,
+	name varchar(20),
+	age integer,
+	kind varchar(20),
+	dob date,
+	location varchar(50)
+);
+
+TABLE animals;
+
+INSERT INTO animals (name, age, kind, dob, location)
+VALUES ('Cat', 45, 'tiger', '2010-10-10', 'Seoul, Korea'),
+	('Pooh', 13, 'bear', '1931-01-04', 'England'),
+	('Togo', 13, 'dog', '1897-12-14','Alaska, USA'),
+	('Michealangelo', 12, 'turtle', '1981-04-05', 'USA'),
+	('Baghera', 27, 'pather', '1888-12-12','England');
+
